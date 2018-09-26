63dc49154cf952e408556fb2b7f50aeed6da2467
liuht <liuhuitao890411>
2018-09-26 11:45:47 +0800

    2.x修改

---
+++ src/views/service/admin/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <d2-container-iframe :src="src"/>
+</template>
+<script>
+export default {
+  name: 'Taroco-Admin',
+  data () {
+    return {
+      src: `${process.env.VUE_APP_ADMIN_URL}`
+    }
+  }
+}
+</script>
